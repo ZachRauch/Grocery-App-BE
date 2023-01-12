fd451dda21edd63b724a5a745ba229eaaf95706b
ZachRauch <zach.rauch@outlook.com>
2023-01-12 09:29:38 -0600

    first push

---
+++ src/main/java/net/yorksolutions/zachrauchpantrybe/ZachRauchPantryBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.zachrauchpantrybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZachRauchPantryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZachRauchPantryBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/zachrauchpantrybe/ZachRauchPantryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.zachrauchpantrybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZachRauchPantryBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
